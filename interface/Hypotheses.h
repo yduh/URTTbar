f316794739bd25e7d5e2422b2fa6b7f398517be8
Mauro Verzetti <mverzett@cern.ch>
2015-03-05 08:29:07 -0600

    add masses calculation

--- interface/Hypotheses.h
+++ interface/Hypotheses.h
@@ -28,12 +28,23 @@ WHypothesis():
 			return false;
 		}
 	}
+
+	TLorentzVector p4() const {
+		if(hasMissingProng()) return TLorentzVector();
+		return (*first + *second);
+	}
+
+	double mass() const {
+		if(hasMissingProng()) return -1;
+		return p4().M();
+	}
 };
 
 enum DecayType {NOTSET, INVALID, FULLHAD, SEMILEP, FULLLEP};
 struct TTbarHypothesis {
-TTbarHypothesis():
-	b(0), 
+	TTbarHypothesis():
+		b(0), 
+		chisq(-1),
 		bbar(0),
 		wplus(), 
 		wminus(),
@@ -42,15 +53,26 @@ TTbarHypothesis():
 	const TLorentzVector *b, *bbar;
 	WHypothesis wplus, wminus;
 	DecayType decay;
+	double chisq;
 	bool hasMissingProng() const {
 		return (!b || !bbar || wplus.hasMissingProng() || wminus.hasMissingProng());
 	}
+
 	bool matches(const TTbarHypothesis& other, float threshold=0.3){
 		if(!b || !other.b || b->DeltaR(*other.b) > threshold) return false;
 		if(!bbar || !other.bbar || bbar->DeltaR(*other.bbar) > threshold) return false;
 		if(!wplus.matches( other.wplus , threshold)) return false;
 		if(!wminus.matches(other.wminus, threshold)) return false;
 		return true;
+	}
+
+	double top_mass() const {
+		if(wplus.hasMissingProng()) return -1;
+		return (wplus.p4() + *b).M();
+	}
+	double tbar_mass() const {
+		if(wminus.hasMissingProng()) return -1;
+		return (wminus.p4() + *bbar).M();
 	}
 };
 
