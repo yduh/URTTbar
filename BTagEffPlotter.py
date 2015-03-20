e77cafd78126bbeca9d64bb6743b278939717ebb
Mauro Verzetti <mverzett@cern.ch>
2015-03-20 02:52:45 -0500

    add more and more plots

--- BTagEffPlotter.py
+++ BTagEffPlotter.py
@@ -11,6 +11,11 @@ views = plotting.views
 from array import array
 import ROOT
 from pdb import set_trace
+import rootpy
+from fnmatch import fnmatch
+rootpy.log["/"].setLevel(rootpy.log.INFO)
+ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetOptStat(0)
 
 class BTagPlotter(Plotter):
    def __init__(self):
@@ -26,34 +31,34 @@ class BTagPlotter(Plotter):
       self.views['ttJets_allRight'] = {
          'view' : self.create_tt_subsample(
             'semilep_visible_right', 
-            'tt, right combination',
+            'tt, right cmb',
             '#6666b3'
             )
          }
       self.views['ttJets_rightHad'] = {
          'view' : self.create_tt_subsample(
             'semilep_right_thad', 
-            'tt, right had top',
+            'tt, right t_{h}',
             ),
          }
       self.views['ttJets_rightWHad'] = {
          'view' : self.create_tt_subsample(
             'semilep_right_whad', 
-            'tt, right had W',
+            'tt, right W_{h}',
             '#cccce6'
             )
          }
       self.views['ttJets_rightWLep'] = {
          'view' : self.create_tt_subsample(
             'semilep_right_tlep', 
-            'tt, right lep W',
+            'tt, right W_{l}',
             '#aac0d5'
             )
          }
       self.views['ttJets_semiWrong'] = {
          'view' : self.create_tt_subsample(
             'semilep_wrong', 
-            'tt, wrong combination',
+            'tt, wrong cmb',
             '#88a7c4'
             )
          }
@@ -89,8 +94,112 @@ class BTagPlotter(Plotter):
          linecolor = color
          )
 
-   def bake_pie(self, var):
-      var = 'all/%s/leading/all/pflav_smart' % var
+   def write_mass_discriminant_shapes(self, folder, rebin=1, preprocess=None):
+      folder.cd()
+      path = os.path.join(folder, 'mass_discriminant')
+      ritghtW_view = views.SumView(
+         self.get_view('ttJets_allRight'),
+         self.get_view('ttJets_rightHad'),
+         self.get_view('ttJets_rightWHad')
+         )
+      wrongW_view = views.SumView(
+         self.get_view('ttJets_rightWLep'),
+         self.get_view('ttJets_semiWrong')
+         )
+      other_view = self.get_view('ttJets_other')
+      singlet_view = self.get_view('single*')
+
+      if preprocess:
+         ritghtW_view = preprocess(ritghtW_view)
+         wrongW_view  = preprocess(wrongW_view )
+         other_view   = preprocess(other_view  )
+         singlet_view = preprocess(singlet_view)
+
+      shape_views = [ritghtW_view, wrongW_view,
+                     other_view, singlet_view]
+      shape_names = ['right_whad', 'wrong_whad'
+                     'nonsemi_tt', 'single_top']
+      for view, name in zip(shape_views, shape_names):
+         histo = view.Get(path)
+         histo.SetName(name)
+         histo.Write()
+
+   def plot_mc_shapes(self, folder, variable, rebin=1, xaxis='',
+                      leftside=True, xrange=None, preprocess=None,
+                      normalize=False, logy=False, filt=None, show_err=False):
+      mc_views = None
+      if not filt:
+         mc_views = self.mc_views(rebin, preprocess, folder)
+      else:
+         mc_views = [views.SubdirectoryView(
+               self.rebin_view(
+                  self.get_view(i), 
+                  rebin
+                  ), 
+               folder
+               ) for i in filt]
+         if preprocess:
+            mc_views = [preprocess(i) for i in mc_views]
+
+      histos = [i.Get(variable) for i in mc_views]
+
+      if normalize:
+         #set_trace()
+         for i in histos:
+            if i.Integral():
+               i.Scale(1./i.Integral())
+      m = 0.
+      first = True
+      for hist in histos:
+         hist.fillstyle = 'hollow'
+         hist.legendstyle = 'l'
+         hist.linewidth = 2
+         if show_err:
+            hist.drawstyle = 'pe'
+            hist.markerstyle = 20
+            hist.legendstyle = 'pe'
+            hist.markercolor = hist.linecolor
+
+         hist.GetXaxis().SetTitle(xaxis)
+         if first:
+            hist.Draw()
+            first = False
+         else:
+            hist.Draw('same')
+         
+         self.keep.append(hist)
+         m = max(m,max(bin for bin in hist))
+      histos[0].GetYaxis().SetRangeUser(0., m*1.2)
+      if xrange:
+         histos[0].GetXaxis().SetRangeUser(*tuple(xrange))
+      self.add_legend(histos, leftside)
+   
+   def make_flavor_table(self, histo, fname):
+      ids = {
+         0 : 'pile-up',
+         1 : 'down',
+         2 : 'up',
+         3 : 'strange',
+         4 : 'charm',
+         5 : 'beauty',
+         6 : 'top',
+         21: 'gluon',            
+         }
+      format = '%10s %10s %10s\n'
+
+      with open(os.path.join(self.outputdir, fname), 'w') as f:
+         total = histo.Integral()
+         f.write('Total events: %.0f\n' % total)
+         header = format % ('quarks', 'yield', 'relative')
+         f.write(header)
+         f.write('-'*len(header)+'\n')
+         for idx, name in ids.iteritems():
+            entries  = histo.GetBinContent(histo.FindBin(idx))
+            entries += histo.GetBinContent(histo.FindBin(-idx))
+            f.write(format % (name, '%.0f' % entries, '%.0f%%' % ((entries/total)*100)))
+      
+   def bake_pie(self, path):
+      var = '%s/pflav_smart' % path
       mc_views = self.mc_views()
       mc_histos = [i.Get(var) for i in mc_views]
       integrals = [i.Integral() for i in mc_histos]
@@ -99,7 +208,7 @@ class BTagPlotter(Plotter):
       names = [i.GetTitle() for i in mc_histos]
       colors = [i.GetFillColor('root') for i in mc_histos]
       #set_trace()
-      format = '%30s %10s %10s\n'
+      format = '%20s %10s %10s\n'
       with open(os.path.join(self.outputdir, 'yields.raw_txt'), 'w') as f:
          f.write('Total events: %.0f\n' % total)
          header = format % ('process', 'yield', 'relative')
@@ -108,81 +217,70 @@ class BTagPlotter(Plotter):
          for name, entries, ratio in zip(names, integrals, ratios):
             f.write(format % (name, '%.0f' % entries, '%.0f%%' % (ratio*100)))
 
-      with open(os.path.join(self.outputdir, 'flavors.raw_txt'), 'w') as f:
-         f.write('Total events: %.0f' % total)
-         header = format % ('quanks', 'yield', 'relative')
-         f.write(header)
-         f.write('-'*len(header)+'\n')
-         htotal = sum(mc_histos)
-         ids = {
-            0 : 'pile-up',
-            1 : 'down',
-            2 : 'up',
-            3 : 'strange',
-            4 : 'charm',
-            5 : 'beauty',
-            6 : 'top',
-            21: 'gluon',
-            }
+      hsum = sum(mc_histos)
+      self.make_flavor_table(hsum, 'flavors.raw_txt')
 
-         for idx, name in ids.iteritems():
-            entries  = htotal.GetBinContent(htotal.FindBin(idx))
-            entries += htotal.GetBinContent(htotal.FindBin(-idx))
-            f.write(format % (name, '%.0f' % entries, '%.0f%%' % ((entries/total)*100)))
-      ## pie = ROOT.TPie('pie','', len(ratios), array('d', ratios), array('i', colors))
-      ## #pie.SetRadius(.15)
-      ## self.pad.Divide(2)
-      ## p1 = self.pad.cd(1)
-      ## p1.Draw()
-      ## self.keep.append(p1)
-      ## for idx, _ in enumerate(ratios):
-      ##    pie.SetEntryLabel(idx, '')
-      ## self.keep.append(pie)
-      ## pie.Draw('nol')
-      ## 
-      ## p2 = self.pad.cd(2)
-      ## p2.Draw()
-      ## self.keep.append(p2)
-      ## for h, ratio in zip(mc_histos, ratios):
-      ##    h.title += ' (%.0f%%)' % ratio
-      ## self.add_legend(mc_histos, False)
-      
+      right_cmb = self.get_view('ttJets_allRight').Get(var)
+      self.make_flavor_table(right_cmb, 'flavors_rightcmb.raw_txt')
+
+      wright_view = views.SumView(
+         *[self.get_view(i) for i in ['ttJets_rightHad', 'ttJets_rightWHad']]
+          )
+      wright = wright_view.Get(var) + right_cmb
+      self.make_flavor_table(wright, 'flavors_rightw.raw_txt')
    
 plotter = BTagPlotter()
 
-main = ['leading', 'subleading']
-sub  = ["tagged", "all"]
 jet_variables = [
-   ('pt' ,  10, '%s jet p_{T}'),
-   ('eta',   1, '%s jet #eta'),
-   ('phi',  10, '%s jet #varphi'),
-   ('pflav', 1, '%s jet parton flavour'),
-   ('pflav_smart', 1, '%s jet parton flavour'),
+   ('pt' ,  10, '%s jet p_{T} (GeV)', None),
+   ('eta',   1, '%s jet #eta', None),
+   ('phi',  10, '%s jet #varphi', None),
+   ('pflav', 1, '%s jet parton flavour', [-7, 22]),
+   ('pflav_smart', 1, '%s jet parton flavour', [-7, 22]),
+   ("ncharged", 1, "%s jet charged constituents", None),
+   ("nneutral", 1, "%s jet neutral constituents", None),
+   ("ntotal"  , 1, "%s jet total constituents", None),
 ]
 
 variables = [
-  ("njets"    , "# of selected jets", 1),
-  ("nbjets"   , "# of bjets", 1),
-  ("b_pt"     , "p_{T}(b) (GeV)", 10),
-  ("bbar_pt"  , "p_{T}(b) (GeV)", 10),
-  ("lep_pt"   , "p_{T}(#ell) (GeV)", 10),
-  #("Wlep_mass", "m_{W}(lep) (GeV)", 10),
-  ("Whad_mass", "m_{W}(had) (GeV)", 10),
-  ("thad_mass", "m_{t}(had) (GeV)", 10),
-  ("nu_chisq"         , "nu_chisq"         , 1),
-	("nu_discriminant"	, "nu_discriminant"	 , 1),
-	("btag_discriminant", "btag_discriminant", 1),
-	("mass_discriminant", "mass_discriminant", 1),
-	("full_discriminant", "full_discriminant", 1),
+  ("njets"    , "# of selected jets", 1, [0, 12]),
+  ("nbjets"   , "# of bjets", 1, [0, 12]),
+  ("b_pt"     , "p_{T}(b) (GeV)", 10, None),
+  ("bbar_pt"  , "p_{T}(b) (GeV)", 10, None),
+  ("lep_pt"   , "p_{T}(l) (GeV)", 10, None),
+  #("Wlep_mass", "m_{W}(lep) (GeV)", 10, None),
+  ("Whad_mass", "m_{W}(had) (GeV)", 10, None),
+  ("Whad_DR"  , "#DeltaR(jj) W_{had} (GeV)", 1, [0,7]),
+  ("Whad_pt"  , "p_{T}(W_{had}) (GeV)", 10, None),
+  ("Whad_leading_DR", "#DeltaR(W_{had}, leading jet)"   , 1, [0,7]),
+  ("Whad_sublead_DR", "#DeltaR(W_{had}, subleading jet)", 1, [0,7]),
+  ("thad_mass", "m_{t}(had) (GeV)", 10, None),
+  ("nu_chisq"         , "nu_chisq"         , 1, [0, 20]),
+	("nu_discriminant"	, "nu_discriminant"	 , 1, None),
+	("btag_discriminant", "btag_discriminant", 1, [-11, -2]),
+	("mass_discriminant", "mass_discriminant", 2, [1, 7]),
+	("full_discriminant", "full_discriminant", 1, None),
 ]
 
+shapes = set([
+      'ncharged', 'nneutral', 'ntotal', 'mass_discriminant', 
+      'full_discriminant', 'Whad_sublead_DR', 'Whad_pt', 
+      'Whad_leading_DR', 'Whad_DR'
+])
+
 orders = [
-   "nu_chisq"         , 
-   "nu_discriminant"  , 
-   "btag_discriminant", 
+   ## "nu_chisq"         , 
+   ## "nu_discriminant"  , 
+   ## "btag_discriminant", 
    "mass_discriminant", 
    "full_discriminant",
 ]
+
+jet_categories = [
+   "all", "lead_tagged", "sublead_tagged", 
+   "both_tagged", "both_untagged"
+]
+jet_types = ['leading', 'subleading']
 
 def set_pdg_bins(histo):
    ids = {
@@ -204,46 +302,84 @@ additional_opts = {
    'btag_discriminant' : {
       'xrange' : [-11, -2]
       },
-   'pflav' : {
-      'xrange' : [-7, 22],
-      #'preprocess' : PDGIdView,
-      },
-   'nbjets' : { 'xrange' : [0, 12] },
-   'njets' : { 'xrange' : [0, 12] },
    'nu_chisq' : {'xrange' : [0, 20]},
 }
 
 for order in orders:
-   base = os.path.join('all', order)
-   for m in main:
-      for s in sub:    
-         folder = os.path.join(base, m, s)
-         plotter.set_subdir(os.path.join(order, m, s))
-         for var, rebin, axis in jet_variables:
+   for jet_cat in jet_categories:
+      base = os.path.join('all', order, jet_cat)
+      for jtype in jet_types:
+         folder = os.path.join(base, jtype)
+         plotter.set_subdir(os.path.join(order, jet_cat, jtype))
+         for var, rebin, axis, x_range in jet_variables:
             plotter.plot_mc_vs_data(
                folder, var, rebin, sort=True,
-               xaxis=axis % m, leftside=False, 
-               **additional_opts.get(var,{}))
+               xaxis=axis % jtype, leftside=False, 
+               xrange=x_range)
             if 'pflav' in var:
                for h in plotter.keep:
                   if isinstance(h, plotting.HistStack):
                      set_pdg_bins(h)
-            plotter.save('_'.join((m,s,var)), pdf=False)
-   plotter.set_subdir(order)
-   
-   plotter.bake_pie(order)
-   #plotter.save('contributions', pdf=False)
+            plotter.save('_'.join((jtype,var)), pdf=False)
+            if var in shapes:
+               plotter.plot_mc_shapes(
+                  folder, var, rebin,
+                  xaxis=axis % jtype, leftside=False,
+                  xrange=x_range)
+               plotter.save('shape_'+'_'.join((jtype,var)), pdf=False)
+         plotter.bake_pie(folder)
 
-   for var, axis, rebin in variables:
-      plotter.plot_mc_vs_data(
-         base, var, rebin, sort=True,
-         xaxis=axis, leftside=False,
-         **additional_opts.get(var,{}))
-      plotter.save(var, pdf=False)
+      plotter.set_subdir(os.path.join(order, jet_cat))
+      for var, axis, rebin, x_range in variables:
+         plotter.plot_mc_vs_data(
+            base, var, rebin, sort=True,
+            xaxis=axis, leftside=False,
+            xrange=x_range)
+         plotter.save(var, pdf=False)
+         if var in shapes:
+            if var == "mass_discriminant" and "_tagged" in jet_cat: rebin = 4
+            plotter.plot_mc_shapes(
+               base, var, rebin,
+               xaxis=axis, leftside=False,
+               xrange=x_range)
+            plotter.save('shape_%s' % var, pdf=False)
+            if var <> "mass_discriminant": continue
+            plotter.plot_mc_shapes(
+               base, var, rebin, normalize=True,
+               xaxis=order.replace('_', ' '), leftside=False,
+               xrange=x_range)
+            plotter.save('normshape_%s' % order, pdf=False)
+
+            plotter.plot_mc_shapes(
+               base, var, rebin, normalize=True, show_err=True,
+               xaxis=order.replace('_', ' '), leftside=False,
+               xrange=x_range, filt=['ttJets_allRight', 'ttJets_rightHad', 'ttJets_rightWHad'])
+            plotter.save('normshape_rightwhad_%s' % order, pdf=False)
+
+            plotter.plot_mc_shapes(
+               base, var, rebin, normalize=True, show_err=True,
+               xaxis=order.replace('_', ' '), leftside=False,
+               xrange=x_range, filt=['ttJets_rightWLep', 'ttJets_semiWrong'])
+            plotter.save('normshape_wrongwhad_%s' % order, pdf=False)
 
    plotter.set_subdir("discriminants")
-   plotter.plot_mc_vs_data(
-      'all/discriminators', order, 1, sort=True,
+   ## plotter.plot_mc_vs_data(
+   ##    'all/discriminators', order, 1, sort=True,
+   ##    xaxis=order.replace('_', ' '), leftside=False,
+   ##    **additional_opts.get(order,{}))
+   ## plotter.save(order, pdf=False)
+   plotter.plot_mc_shapes(
+      'all/discriminators', order, 1,
       xaxis=order.replace('_', ' '), leftside=False,
       **additional_opts.get(order,{}))
-   plotter.save(order, pdf=False)
+   plotter.save('shape_%s' % order, pdf=False)
+
+   plotter.plot_mc_shapes(
+      'all/discriminators', order, 1, normalize=True,
+      xaxis=order.replace('_', ' '), leftside=False,
+      **additional_opts.get(order,{}))
+   plotter.save('normshape_%s' % order, pdf=False)
+
+      
+
+#write_mass_discriminant_shapes(self, folder, rebin=1, preprocess=None):
