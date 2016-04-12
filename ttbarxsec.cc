0e09c748592b70f7da0d752b31a6a9ab7bb68cbb
Yi-Ting Duh <kaliduh@gmail.com>
2016-04-12 10:01:29 -0500

    results/test

--- ttbarxsec.cc
+++ ttbarxsec.cc
@@ -1848,7 +1848,7 @@ void ttbar::ttanalysis(URStreamer& event)
         reco3j2d["Mtt_delY"]->Fill((tlep_3j + thad_3j).Mag(), tlep_3j.Rapidity()-thad_3j.Rapidity(), weight);
 
 
-    if(rightper.BLep() == bleper && rightper.BHad() == bhadper){  
+    if(rightper.BLep() == bleper && rightper.BHad() == bhadper && rightper.IsComplete3J()){  
     //if(rightper.IsComplete3J()){
         truth3j2d["select_bchi2"]->Fill(chi2candidate1, chi2candidate2, weight);
         truth3j2d["select_bcsv"]->Fill(reducedjets[0]->csvIncl(), reducedjets[1]->csvIncl(), weight);
