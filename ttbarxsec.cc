69c2cb768c09873d2cfb2dedcb0b51f7dae78680
Yi-Ting Duh <kaliduh@gmail.com>
2016-04-20 04:23:33 -0500

    results/stage5/test

--- ttbarxsec.cc
+++ ttbarxsec.cc
@@ -1785,7 +1785,7 @@ void ttbar::ttanalysis(URStreamer& event)
 	//cut on number of jets
 	reco1d["jetmulti"]->Fill(cleanedjets.size(), weight);
 //cout << "NC: " << cleanedjets.size() << endl;
-	if(cleanedjets.size() != 3){return;} // change for 3j test
+	if(cleanedjets.size() < 3){return;} // change for 3j test
 	reco1d["c_jets"]->Fill(event.run+0.5);
 	if(BTAGMODE && cleanedjets.size() > 4){return;}
 	reco1d["counter"]->Fill(2.5, weight);
