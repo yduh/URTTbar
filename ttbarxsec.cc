0c5b874b7176116065879683a4c9815fcf178dd8
Yi-Ting Duh <kaliduh@gmail.com>
2016-05-18 09:26:53 -0500

    results/3j/3.0y

--- ttbarxsec.cc
+++ ttbarxsec.cc
@@ -836,7 +836,7 @@ void ttbar::begin()
 	//TFile* fyuka_beta = TFile::Open("yukawa2_beta.root");
 	//yukahist_beta = (TH1D*)fyuka_beta->Get("XSR_beta");
 	
-        TFile* fyuka_2d = TFile::Open("yukawa_reweighing2.5.root");
+        TFile* fyuka_2d = TFile::Open("yukawa_reweighing3.0.root");
 	yukahist_2d = (TH2D*)fyuka_2d->Get("EWtoLO");
 
 
