5488d72ad8d32d480aa63f1b28ae1c451a34cfb6
Yi-Ting Duh <kaliduh@gmail.com>
2016-07-28 12:31:11 -0500

    results/4j/1.0y_offshell/

--- ttbarxsec.cc
+++ ttbarxsec.cc
@@ -558,7 +558,7 @@ void ttbar::begin()
         yuka1d_offshell.AddHist("Mtbar", 250, 0, 500, "offshell M(t) check", "Events");
         yuka1d_offshell.AddHist("pt", 250, 0, 500, "offshell pt(t)", "Events");
         yuka1d_offshell.AddHist("pz", 500, 0, 1000, "offshell pz(t)", "Events");
-        yuka1d_offshell.AddHist("diffweight", 100, 0.5, 1.5, "weight", "Events");
+        yuka2d_offshell.AddHist("diffweight", 1000, 0, 2000, 1200, -6, 6, "weight", "Events");
 	yuka2d_offshell.AddHist("Mtt_delY", 1000, 0, 2000, 1200, -6, 6, "new offshell M(t#bar{t})", "offshell #Deltay_{t#bar{t}}");
 
 	TDirectory* dir_yukawareco = outFile_.mkdir("YUKAWA_RECO");
