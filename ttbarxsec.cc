cf5dcc613426023ef9dfbe19953b1dd127c7f520
Yi-Ting Duh <kaliduh@gmail.com>
2016-07-29 08:25:34 -0500

    results/4j/0.0y_offshell/

--- ttbarxsec.cc
+++ ttbarxsec.cc
@@ -2374,7 +2374,7 @@ void ttbar::analyze()
                                     yuka1d_offshell["pz"]->Fill(v1.Pz(), weight);
                                     yuka1d_offshell["pz"]->Fill(v2.Pz(), weight);
                                     weight2 *= yukahist_2d->GetBinContent(yukahist_2d->GetXaxis()->FindFixBin((v1+v2).M()), yukahist_2d->GetYaxis()->FindFixBin(deltaY)) + 1;
-                                    yuka2d_offshell["Mtt_delY"]->Fill((v1+v2).M(), deltaY, weight2);
+                                    yuka2d_offshell["newMtt_delY"]->Fill((v1+v2).M(), deltaY, weight2);
                                     //yuka1d_offshell["weight"]->Fill(yukahist_2d->GetBinContent(yukahist_2d->GetXaxis()->FindFixBin((v1+v2).M()), yukahist_2d->GetYaxis()->FindFixBin(deltaY)) + 1, weight);
                                     yuka2d_offshell["diffweight"]->Fill(Mtt, deltaY, yukahist_2d->GetBinContent(yukahist_2d->GetXaxis()->FindFixBin((v1+v2).M()), yukahist_2d->GetYaxis()->FindFixBin(deltaY)) - yukahist_2d->GetBinContent(yukahist_2d->GetXaxis()->FindFixBin(Mtt), yukahist_2d->GetYaxis()->FindFixBin(deltaY)));
 
