179ee961114acd5327eee09061003e0415628c80
Yi-Ting Duh <kaliduh@gmail.com>
2016-11-07 16:58:19 -0600

    results/4j_ICHEP/1.0y/mtup

--- rununc.sh
+++ rununc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TYP=results/5j_ICHEP
-TYPUNC=results/5junc_ICHEP
+TYP=results/4j_ICHEP
+TYPUNC=results/4junc_ICHEP
 JOBDIR=JOB12
 #GT='0.0y 1.0y 2.0y 3.0y 4.0y 5.0y N1.0y N2.0y N3.0y N4.0y N5.0y'
 #GT='0.0y 2.0y 3.0y 4.0y 5.0y'
