d05a31dd8be22802403c940cdc79c96b58b10c6c
Yi-Ting Duh <kaliduh@gmail.com>
2016-12-07 12:03:46 -0600

    results/3junc_ICHEP/1.0y/jetm1sig

--- rununc.sh
+++ rununc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TYP=results/totj_ICHEP_implement
+TYP=results/3j_ICHEP_implement
 TYPUNC=results/3junc_ICHEP
 JOBDIR=JOB12
 #GT='0.0y 1.0y 2.0y 3.0y 4.0y 5.0y N1.0y N2.0y N3.0y N4.0y N5.0y'
 #GT='0.0y 2.0y 3.0y 4.0y 5.0y'
 GT='1.0y'
 
-RUN=true
-RUNOUNC=false
+RUN=false
+RUNOUNC=true
 
 
 if $RUN; then
