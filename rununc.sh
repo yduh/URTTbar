c8a1d17a4d7c13a11e44fd9e8fc2e084d70cfcbd
Yi-Ting Duh <kaliduh@gmail.com>
2017-01-10 11:17:59 -0600

    results/newdataset/3j/1.0y

--- rununc.sh
+++ rununc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TYP=results/newdataset/5j
-TYPUNC=results/5junc
+TYP=results/newdataset/3j
+TYPUNC=results/3junc
 JOBDIR=JOB13
 #GT='0.0y 1.0y 2.0y 3.0y 4.0y 5.0y N1.0y N2.0y N3.0y N4.0y N5.0y'
 #GT='0.0y 1.0y 2.0y 3.0y 4.0y 5.0y'
