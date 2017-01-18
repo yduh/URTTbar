927c47df81ff672cc51dc6a374f7540451ccb58d
Yi-Ting Duh <kaliduh@gmail.com>
2017-01-18 03:43:58 -0600

    results/3junc/1.0y/mtDown

--- rununc.sh
+++ rununc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TYP=results/4j
-TYPUNC=results/4junc
+TYP=results/3j
+TYPUNC=results/3junc
 JOBDIR=JOB13
 #GT='0.0y 1.0y 2.0y 3.0y 4.0y 5.0y N1.0y N2.0y N3.0y N4.0y N5.0y'
 #GT='0.0y 2.0y 3.0y 4.0y 5.0y'
@@ -48,13 +48,13 @@ if $RUNMASS; then
     #rm inputs/$JOBDIR/*txt
     #cp inputs/$JOBDIR/backup_theoreticaluncert/mtop/tt_mtop1695_PowhegP8.txt inputs/$JOBDIR
     #cp inputs/$JOBDIR/INPUT_TEMP/yukawa_reweighting${gt}_169.5.root inputs/$JOBDIR/INPUT
-    ./updateconfig.py yukawatxt yukawa_reweighting${gt}_169.5.root 
+    ./updateconfig.py yukawatxt yukawa_reweighting1.0y_169.5.root 
     ./jobsub ${TYPUNC}/1.0y/mtDown ttbarxsec.exe ttbarxsec.cfg
 
     #rm inputs/$JOBDIR/*txt
     #cp inputs/$JOBDIR/backup_theoreticaluncert/mtop/tt_mtop1755_PowhegP8.txt inputs/$JOBDIR
     #cp inputs/$JOBDIR/INPUT_TEMP/yukawa_reweighting${gt}_175.5.root inputs/$JOBDIR/INPUT
-    #./updateconfig.py yukawatxt yukawa_reweighting${gt}_175.5.root 
+    #./updateconfig.py yukawatxt yukawa_reweighting1.0y_175.5.root 
     #./jobsub ${TYPUNC}/1.0y/mtUp ttbarxsec.exe ttbarxsec.cfg
 
     mv ttbarxsec.tmp ttbarxsec.cfg
