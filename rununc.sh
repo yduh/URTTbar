234f198a875d823e2e11d0fb8ec0f8c33ca4945e
Yi-Ting Duh <kaliduh@gmail.com>
2017-01-18 04:03:31 -0600

    results/6junc/1.0y/mtUp

--- rununc.sh
+++ rununc.sh
@@ -48,14 +48,14 @@ if $RUNMASS; then
     #rm inputs/$JOBDIR/*txt
     #cp inputs/$JOBDIR/backup_theoreticaluncert/mtop/tt_mtop1695_PowhegP8.txt inputs/$JOBDIR
     #cp inputs/$JOBDIR/INPUT_TEMP/yukawa_reweighting${gt}_169.5.root inputs/$JOBDIR/INPUT
-    ./updateconfig.py yukawatxt yukawa_reweighting1.0y_169.5.root 
-    ./jobsub ${TYPUNC}/1.0y/mtDown ttbarxsec.exe ttbarxsec.cfg
+    #./updateconfig.py yukawatxt yukawa_reweighting1.0y_169.5.root 
+    #./jobsub ${TYPUNC}/1.0y/mtDown ttbarxsec.exe ttbarxsec.cfg
 
     #rm inputs/$JOBDIR/*txt
     #cp inputs/$JOBDIR/backup_theoreticaluncert/mtop/tt_mtop1755_PowhegP8.txt inputs/$JOBDIR
     #cp inputs/$JOBDIR/INPUT_TEMP/yukawa_reweighting${gt}_175.5.root inputs/$JOBDIR/INPUT
-    #./updateconfig.py yukawatxt yukawa_reweighting1.0y_175.5.root 
-    #./jobsub ${TYPUNC}/1.0y/mtUp ttbarxsec.exe ttbarxsec.cfg
+    ./updateconfig.py yukawatxt yukawa_reweighting1.0y_175.5.root 
+    ./jobsub ${TYPUNC}/1.0y/mtUp ttbarxsec.exe ttbarxsec.cfg
 
     mv ttbarxsec.tmp ttbarxsec.cfg
 fi
